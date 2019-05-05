42635e3aed008d3547e8a4808a0ee7af815fd899
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-05 06:28:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   squared_array = []
   array.each do |element|
-    squared = element**2
-    squared_array << squared
+    squared_array << element**2
   end
   squared_array
 end
